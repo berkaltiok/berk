7645675cb59213b3d597f71b0d9b9d23f3a6524c
Berk Altıok <berkpw@gmail.com>
2021-01-05 22:13:23 +0300

    Update chalk and create content json file

--- berk.js
+++ berk.js
@@ -5,16 +5,16 @@
 process.stdout.write("\u001b[3J\u001b[2J\u001b[1J\u001b[3J");
 console.clear();
 
-var terminalImage = require('terminal-image');
-var got           = require('got');
-var ww            = require('word-wrap');
-var c             = require('chalk');
+const terminalImage   =   require('terminal-image');
+const chalk           =   require('chalk');
+const got             =   require('got');
+const ww              =   require('word-wrap');
 
 console.log("")
 got('https://avatars3.githubusercontent.com/u/17373485?s=600&v=4', { responseType: 'buffer' })
   .then((image) => terminalImage.buffer(image.body, { width: '26%' }))
   .then((image) => image.split("\n").forEach((line) => console.log("  " + line)))
   .then(() => {
-    console.log(ww(`Hello, this is ${c.blue.bold("Berk ALTIOK")}!`.trim(), { width: 200, trim: true }));
+    console.log(ww(chalk`Hello, this is {bold.blue Berk ALTIOK!}`, { width: 200, trim: true }));
   })
   .catch((e) => console.log(e));
